1b920c5cab9dd3b92da84b0aaef45a2db2ed1540
Artem <temas1994@mail.ru>
2023-01-10 11:56:41 +0300

    settings

--- frontend/src/features/chat/ModalUserWindow/ModalUserWindow.tsx
+++ frontend/src/features/chat/ModalUserWindow/ModalUserWindow.tsx
@@ -1,35 +1,31 @@
-import React, { useMemo } from "react";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import Modal from "@mui/material/Modal";
-import { TextField } from "@mui/material";
-import { io, Socket } from "socket.io-client";
-import useSocket from "../Hooks/useSocket";
+import React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import { TextField } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { selectCurrentUser } from '../selectors';
 
 function ModalUserWindow(): JSX.Element {
-  const socket: Socket = useMemo(
-    () => io(window.location.origin, { withCredentials: true }),
-    []
-  );
-  const { user } = useSocket(socket);
+  const user = useSelector(selectCurrentUser);
 
   const style = {
-    position: "absolute" as "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
     width: 500,
-    bgcolor: "background.paper",
-    border: "2px solid #000",
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
     boxShadow: 24,
     p: 4,
     height: 500,
     borderRadius: 10,
-    display: "flex",
+    display: 'flex',
     // justifyContent: "flex-start",
-    alignItems: "center",
-    flexDirection: "column",
+    alignItems: 'center',
+    flexDirection: 'column',
     marginBottom: 5,
   };
 
@@ -39,38 +35,39 @@ function ModalUserWindow(): JSX.Element {
 
   return (
     <div>
-      <Button sx={{ color: "black" }} onClick={handleOpen}>
-        Open modal
+      <Button sx={{ color: 'black' }} onClick={handleOpen}>
+        <img
+          style={{ height: 30 }}
+          src='https://cdn-icons.flaticon.com/svg/3917/3917058.svg?token=exp=1673278794~hmac=81b7b5c0c23c1ef6a71ee024c6d33603'
+          alt='icon'
+        />
       </Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
+      <Modal open={open} onClose={handleClose} aria-labelledby='modal-modal-title' aria-describedby='modal-modal-description'>
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
+          <Typography id='modal-modal-title' variant='h6' component='h2'>
             Edit profile
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          <Typography id='modal-modal-description' sx={{ alignItems: 'center', mt: 2, display: 'flex', flexDirection: 'column' }}>
             <Box
               sx={{
                 height: 100,
                 width: 100,
-                borderRadius: "50%",
-                backgroundColor: "#4A95D6",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
+                borderRadius: '50%',
+                backgroundColor: '#4A95D6',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
               }}
             >
               {user?.name[0]}
             </Box>
             <>
-              <TextField sx={{marginTop: 5, marginBottom: 5}} fullWidth label="Nickname" id="fullWidth" value={user?.name} />
-              <TextField fullWidth label="Bio" id="fullWidth" />
-              <Button sx={{marginTop: 5, marginBottom: 5}} variant="contained">Contained</Button>
-              </>
+              <TextField sx={{ marginTop: 5, marginBottom: 5 }} fullWidth label='Nickname' id='fullWidth' value={user?.name} />
+              <TextField fullWidth label='Bio' id='fullWidth' />
+              <Button sx={{ marginTop: 5, marginBottom: 5 }} variant='contained'>
+                Contained
+              </Button>
+            </>
           </Typography>
         </Box>
       </Modal>
