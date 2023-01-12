1dcf2af059b855c6f02923fdbefb14551b0f2e95
cksi <safecript@gmail.com>
2023-01-12 10:58:12 +0300

    AppRouteDeployFix

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,6 @@ const http = require("http");
 
 const io = require("socket.io");
 
-
 const expressConfig = require("./config/express");
 
 const { Message, User } = require("./db/models");
@@ -25,30 +24,33 @@ const friendsRouter = require("./routes/FriendsRouter");
 
 const server = http.createServer(app);
 
-function check(req){
-  if(req.socket.isAuthenicated){
-    return {success:true}
-  }
-  else return {success:false}
+function check(req) {
+  if (req.socket.isAuthenicated) {
+    return { success: true };
+  } else return { success: false };
 }
 
 const ioSocket = new io.Server(server, {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
-    credentials:true
+    credentials: true,
   },
   allowRequest: (req, callback) => {
-try{
-  const isOriginValid = check(req);
-  callback(null, isOriginValid);
-}
-catch(err){
-  console.log(err)
-}  }
+    try {
+      const isOriginValid = check(req);
+      callback(null, isOriginValid);
+    } catch (err) {
+      console.log(err);
+    }
+  },
 });
 
 expressConfig(app, ioSocket);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+});
 
 app.use("/api/auth", authRouter);
 app.use("/api/uploadFile", uploadRouter);
@@ -60,37 +62,29 @@ app.use("/api/friends", friendsRouter);
 app.locals.io = ioSocket;
 
 ioSocket.on("connection", async (socket) => {
-  try{
+  try {
+    socket.join(`User_${socket.userId}room`);
 
-  socket.join(`User_${socket.userId}room`)
+    app.locals.io = ioSocket;
 
-  app.locals.io = ioSocket;
+    socket.on("/messages/send", async (data) => {
+      const { text, chatId } = JSON.parse(data);
 
-
-  socket.on("/messages/send", async (data) => {
-    
-  const {text, chatId} = JSON.parse(data);
-  
-  if(chatId){
-
-  const message = await Message.create({text,username:socket.user.name,chatId});
-    console.log(`Message: ${text} sended to chat${chatId}`);
-  ioSocket.sockets.emit("/messages/recieve", message);
-
-}}
-
-);
-
-
-}
-  catch(error) {
+      if (chatId) {
+        const message = await Message.create({
+          text,
+          username: socket.user.name,
+          chatId,
+        });
+        console.log(`Message: ${text} sended to chat${chatId}`);
+        ioSocket.sockets.emit("/messages/recieve", message);
+      }
+    });
+  } catch (error) {
     console.log(error);
-  }}
-);
-
-
+  }
+});
 
 server.listen(PORT, () => {
   console.log("EXPRESS AND SOCKET SERVERS ARE RUNNING");
 });
-
